e17acd2811a4346e46ff8d29a931ac824926af1f
joshuacitrak <joshuacitrak@gmail.com>
2016-03-24 14:34:02 -0700

    added gitignore file, player controls

---
+++ _/components/js/main.js
@@ -0,0 +1,74 @@
+function startAd(){  
+           
+};
+
+function addListeners(){
+    document.getElementById("bradContent").addEventListener("click", clickthrough);
+};
+
+function clickthrough() {
+    EB.clickthrough();
+}
+
+
+function animationComplete(evt){
+};
+
+function checkInit() {
+              if (!EB.isInitialized()) {
+                 EB.addEventListener(EBG.EventName.EB_INITIALIZED, onInit); 
+                 // This code checks whether the EB object is initialized, if the object is initialized, it
+                     //launches the function "onInit", otherwise "EB_INITIALIZED" event. 
+              }
+              else {
+                      onInit();
+               }         
+               function onInit() {
+    
+    TweenLite.set("#bradContainer", {opacity:1});
+    addListeners();
+    startAd();
+              } 
+     }
+
+window.addEventListener('load', checkInit);
+
+var tl = new TimelineLite({onUpdate:updateSlider});
+tl.eventCallback("onComplete", animationComplete);
+
+
+$("#play").click(function() {
+  //play() only plays forward from current position. If timeline has finished, play() has nowhere to go.
+  //if the timeline is not done then play() or else restart() (restart always restarts from the beginning).
+
+  if(tl.progress() != 1){
+    //carl just changed this again
+		tl.play();
+  } else {
+    tl.restart();
+  }
+});
+		
+$("#pause").click(function() {
+		tl.pause();
+});
+		
+$("#restart").click(function() {
+		tl.restart();
+});		
+	
+$("#slider").slider({
+  range: false,
+  min: 0,
+  max: 100,
+  step:.1,
+  slide: function ( event, ui ) {
+    tl.pause();
+    //adjust the timeline's progress() based on slider value
+    tl.progress( ui.value/100 );
+    }
+});	
+		
+function updateSlider() {
+  $("#slider").slider("value", tl.progress() *100);
+} 	
